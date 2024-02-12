d547a14111fe8894afbf5ab075edccead69b0b24
adel <adel@gmail.com>
2024-02-12 22:20:55 +0200

    assigment 6

--- assigment_6/index.js
+++ assigment_6/index.js
@@ -3,10 +3,10 @@ import bootstrap from './bootstrap.js'
 const app = express()
  const port = 3000
   bootstrap(app,express)
-
+ 
 
 
 
 app.listen(port , ()=> {
-    console.log(`server runing now ${port}`)
+    console.log(`server runing now ${port} ${port}`)
 })
